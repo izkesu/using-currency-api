a0333508fb4d19c59a2f598dfc24e82a9a393009
izkesu <ksingh8354990057@gmail.com>
2024-05-09 17:58:20 +0530

    Add project files.

---
+++ task1/Controllers/CurrencyConversionResult.cs
@@ -0,0 +1,11 @@
+﻿namespace task1.Controllers
+{
+    internal class CurrencyConversionResult
+    {
+        public string FromCurrency { get; set; }
+        public string ToCurrency { get; set; }
+        public decimal Amount { get; set; }
+        public dynamic ConvertedAmount { get; internal set; }
+        public string ErrorMessage { get; internal set; }
+    }
+}
---
+++ task1/Models/CurrencyRate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace task1.Models
+{
+    public class CurrencyRate
+    {
+        public string BaseCurrency { get; set; }
+        public Dictionary<string, decimal> Rates { get; set; }
+    }
+}
